d5ed23d38355aba0b027e28f676b994db5ce8e79
Andrea.DeStefano <enricoandrea.destefano@gmail.com>
2022-04-08 15:31:11 +0200

    Milestone 3 - Fne

--- js/script.js
+++ js/script.js
@@ -2,16 +2,23 @@
 const container = document.querySelector('.container'); 
 
 for(i=1; i<=100; i++){
+  const box = document.createElement('div')
+  box.className = 'box'
+  console.log(box);
   const stampa = document.createElement('h2')
   if(i % 3 === 0 && i % 5 === 0){
-    stampa.innerText = 'FizzBuzz';    
+    stampa.innerText = 'FizzBuzz'; 
+    box.classList.add('fizz-buzz');   
   }else if (i % 5 === 0){
     stampa.innerText ='Buzz';
+    box.classList.add('buzz');
   }else if (i % 3 === 0){
     stampa.innerText = 'Fizz';
+    box.classList.add('fizz');
   }else{
       stampa.innerText = i;
   }
-  console.log(stampa);
-  container.append(stampa);
+  
+  box.append(stampa);
+  container.append(box);
 }
